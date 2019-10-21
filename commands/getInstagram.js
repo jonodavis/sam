ed612cdd61ed161ab9370feb91bc4438d16194f7
Jono <jono.davis66@gmail.com>
2019-10-21 23:47:01 +1300

    formatting

--- commands/getInstagram.js
+++ commands/getInstagram.js
@@ -1,16 +1,17 @@
-const rp = require('request-promise')
-const url = 'https://www.instagram.com/'
+const rp = require("request-promise");
+const url = "https://www.instagram.com/";
 
 const getInstagram = (username, callback) => {
-  rp(url + username).then( html => {
-    //success!
-    let chopStart = html.substring(html.indexOf("<meta content=") + 15)
-    let followerCount = chopStart.substring(0, chopStart.indexOf(" "))
-    return callback(followerCount)
-  }).catch( err => {
-    throw err
-  })
-}
+  rp(url + username)
+    .then(html => {
+      //success!
+      let chopStart = html.substring(html.indexOf("<meta content=") + 15);
+      let followerCount = chopStart.substring(0, chopStart.indexOf(" "));
+      return callback(followerCount);
+    })
+    .catch(err => {
+      throw err;
+    });
+};
 
-module.exports = getInstagram
-
+module.exports = getInstagram;
